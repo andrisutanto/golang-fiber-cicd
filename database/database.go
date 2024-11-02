6c00a556306c6ca66a376ddb76feac764cb90b11
Andri Sutanto <me@andrisutanto.com>
2024-11-02 22:06:05 +0800

    update to env

--- database/database.go
+++ database/database.go
@@ -3,7 +3,10 @@ package database
 import (
 	"fmt"
 	"golang-fiber-cicd/models"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,7 +14,22 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "golang-fiber-cicd:piDKpyxKEXya3msh@tcp(172.17.0.1:3306)/golang-fiber-cicd?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get the environment variables for the DSN
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	// Create the DSN (Data Source Name)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPassword, dbHost, dbPort, dbName)
+
 	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database!")
